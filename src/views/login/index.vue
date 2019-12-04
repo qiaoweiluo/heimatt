94c53187f2e44178c2d1de709d81557d9374e846
CCQQ1 <630688213@qq.com>
2019-12-04 11:32:33 +0800

    完成验证逻辑和优化封装

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,20 +6,27 @@
       </template>
     </van-nav-bar>
     <van-cell-group>
-      <van-field v-model="user.mobile" clearable placeholder="请输入手机号">
+      <!-- 手机号输入 -->
+      <van-field
+        v-model="user.mobile"
+        clearable
+        placeholder="请输入手机号"
+        :error-message="yanzheng.mobile"
+      >
         <template slot="left-icon">
           <i class="iconfont icon-phone"></i>
         </template>
       </van-field>
-
-      <van-field v-model="user.code" placeholder="请输入验证码">
+      <!-- 验证码输入 -->
+      <van-field v-model="user.code" placeholder="请输入验证码" :error-message="yanzheng.code">
         <template slot="left-icon">
           <i class="iconfont icon-cc-lock"></i>
         </template>
         <van-button class="mycodebtn" slot="button" size="small">获取验证码</van-button>
       </van-field>
+      <!-- 登录按钮 -->
       <div class="myloginbtn">
-        <van-button class="mybtn" type="info" size="large">登录</van-button>
+        <van-button class="mybtn" @click="login" size="large">登录</van-button>
       </div>
     </van-cell-group>
   </div>
@@ -32,8 +39,44 @@ export default {
       user: {
         mobile: "",
         code: ""
+      },
+
+      yanzheng: {
+        // 验证手机号格式错误提示信息
+        mobile: "",
+        // 验证码格式错误提示信息
+        code: ""
       }
     };
+  },
+  methods: {
+    login() {
+      if (this.checking()) {
+        console.log('验证通过');
+      }
+    },
+    checking() {
+      // 简单的格式验证
+      // 菜单按钮的假设思想取代return来判断是否通过
+      let mobileF = true;
+      let codeF = true;
+      if (this.user.mobile.trim().length !== 11) {
+        this.yanzheng.mobile = "手机号不合法";
+        // return;
+        mobileF = false;
+      } else {
+        this.yanzheng.mobile = "";
+      }
+      if (this.user.code.trim().length !== 6) {
+        this.yanzheng.code = "验证码格式错误";
+        // return;
+        codeF = false;
+      } else {
+        this.yanzheng.code = "";
+      }
+      // 判断是否向下执行
+      return mobileF && codeF
+    }
   }
 };
 </script>
@@ -56,8 +99,9 @@ export default {
   .myloginbtn {
     margin: 40px 20px;
     .mybtn {
-      background-color: #6DB4FB;
+      background-color: #6db4fb;
       border-radius: 10px;
+      color: #fff;
     }
   }
 }
--- src/views/uservant/index.vue
+++ src/views/uservant/index.vue
@@ -24,27 +24,37 @@
       <!-- 通过icon属性在标题左侧展示图标 -->
       <van-cell icon="like" title="左侧的内容" value="右侧的内容" label="描述信息" />
       <!-- 使用插槽 -->
-       <!-- 第一步 把单标签变成双标签 -->
-        <van-cell>
-            <template slot="title">
-                <!-- 上面的title 可以删掉了 会覆盖掉 -->
-                我是左侧的内容 <van-icon name="like" /> 
-            </template>
-            <template slot="default">
-                <!-- 注意 此时的slot的值不是value  -->
-                我是右侧的内容 
-            </template>
-            <template slot="label">
-                我是下方的内容 
-            </template>
-        </van-cell>
-      
+      <!-- 第一步 把单标签变成双标签 -->
+      <van-cell>
+        <template slot="title">
+          <!-- 上面的title 可以删掉了 会覆盖掉 -->
+          我是左侧的内容
+          <van-icon name="like" />
+        </template>
+        <template slot="default">
+          <!-- 注意 此时的slot的值不是value  -->
+          我是右侧的内容
+        </template>
+        <template slot="label">我是下方的内容</template>
+      </van-cell>
+    </van-cell-group>
+    <h3>04 错误提示</h3>
+    <van-cell-group>
+      <van-field v-model="phone" label="手机号" placeholder="请输入手机号" :error-message="errmsg" />
     </van-cell-group>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      phone: '',
+      // 手机号格式错误提示信息
+      errmsg: ""
+    }
+  },
+};
 </script>
 
 <style>
