605029e0415953c944f5e6416109f10a4cd98b04
CCQQ1 <630688213@qq.com>
2019-12-07 22:44:17 +0800

    完成频道列表的初始布局

--- src/components/mychannlepop.vue
+++ src/components/mychannlepop.vue
@@ -3,23 +3,41 @@
     <!-- 弹出层 -->
     <!-- 子组件不能修改这个值 -->
     <!-- <van-popup :value="value" @input="value=$event" position="bottom" :style="{ height: '90%' }">我</van-popup> -->
-    <van-popup :value="value" @input="$emit('input', $event)" position="bottom" :style="{ height: '90%' }">
-        <!-- 我的频道 -->
-        <van-cell title="我的频道">
-            <template slot="default">
-                <van-button plain type='danger' size='mini'>编辑</van-button>
-            </template>
-
-        </van-cell>
-        <!-- 推荐频道 -->
+    <van-popup
+      :value="value"
+      @input="$emit('input', $event)"
+      position="bottom"
+      :style="{ height: '90%' }"
+    >
+      <!-- 我的频道 -->
+      <van-cell title="我的频道">
+        <template slot="default">
+          <van-button plain type="danger" size="mini">编辑</van-button>
+        </template>
+      </van-cell>
+      <!-- 我的频道的列表 -->
+      <van-grid>
+        <van-grid-item icon="photo-o" text="文字" />
+        <van-grid-item icon="photo-o" text="文字" />
+        <van-grid-item icon="photo-o" text="文字" />
+        <van-grid-item icon="photo-o" text="文字" />
+      </van-grid>
+      <!-- 频道推荐 -->
+      <van-cell title="频道推荐"></van-cell>
+      <!-- 频道的列表 -->
+      <van-grid>
+        <van-grid-item icon="photo-o" text="文字" />
+        <van-grid-item icon="photo-o" text="文字" />
+        <van-grid-item icon="photo-o" text="文字" />
+        <van-grid-item icon="photo-o" text="文字" />
+      </van-grid>
     </van-popup>
-    
   </div>
 </template>
 
 <script>
 export default {
-   props:['value']
+  props: ["value"]
 };
 </script>
 
