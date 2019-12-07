98f5d908c71a7e9293a2239c28b5598cc86d3a23
CCQQ1 <630688213@qq.com>
2019-12-07 22:14:19 +0800

    Revert "将频道操作面板单独封装成一个组件完成"
    This reverts commit f48aefaddd05fb80fef18658d614c7f935750955.

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -37,9 +37,10 @@
         <van-icon name="wap-nav" @click="showPop"/>
       </div>
     </van-tabs>
-    <!-- 频道操作面板的弹出层 -->
-    <!-- <mychannlepop :value='show' @input="show=$event" /> -->
-    <mychannlepop v-model="show"/>
+    <!-- 弹出层 -->
+    <!-- v-model: 用来控制面板的显示和隐藏 -->
+    <!-- position: 面板弹出的位置 -->
+    <van-popup v-model="show" position="bottom" :style="{ height: '90%' }"  />
   </div>
 </template>
 
@@ -50,8 +51,6 @@ import { apiGetChannels } from "../../api/channels";
 import { getLocal } from "../../utils/mylocal";
 // 导入文的方法
 import { apiGetChannelsArticel } from "../../api/article";
-// 导入弹出层
-import mychannlepop from '../../components/mychannlepop';
 export default {
   data() {
     return {
@@ -164,9 +163,6 @@ export default {
     showPop() {
       this.show = true
     }
-  },
-  components: {
-    mychannlepop
   },
   // 打开页面就要获取频道页面即 DOM元素创建 created钩子
   created() {
